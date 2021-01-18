a5dfe1e1056c27ca169d092737e1947da5cd87a8
breadnone <64100867+breadnone@users.noreply.github.com>
2021-01-18 22:57:11 +0700

    !st commit

---
+++ TruncateText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TruncateText : MonoBehaviour
+{
+    public Text texts;
+    public int maxSize = 30;
+    public bool reverseTruncate = false;
+    void OnEnable()
+    {
+        if(!reverseTruncate)
+        {
+            if (texts.text.Length > maxSize)
+            {
+                texts.text = texts.text.Substring(0, maxSize) + "...";
+            }
+        }
+        else
+        {
+            if (texts.text.Length > maxSize)
+            {
+                texts.text =  "..." + texts.text.Substring(texts.text.Length - maxSize, maxSize);
+            }
+        }
+    }
+}
